14d61562ec8a1b52c2ceb2d54d2f068ad5d62d8b
AwaabSingh <adeoyin2828@gmail.com>
2022-05-03 16:01:39 +0100

    p

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
-import adapter from '@sveltejs/adapter-node';
 
-export default {
+// import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
-		adapter: adapter()
+		adapter: vercel()
 	}
 };
+
+export default config;
